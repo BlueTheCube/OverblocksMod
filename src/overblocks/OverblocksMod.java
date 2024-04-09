6cbd120ce59f52e1af3a31123ed5c7783569d8c2
Cubical box <calreibozsemimapping@gmail.com>
2024-04-09 12:06:27 +0800

    hot carbon

--- src/overblocks/OverblocksMod.java
+++ src/overblocks/OverblocksMod.java
@@ -1,11 +1,20 @@
 package overblocks;
 
+import mindustry.content.*;
 import mindustry.mod.*;
+import mindustry.world.*;
+import mindustry.world.meta.*;
+import overblocks.content.*;
 import overblocks.gen.*;
 
 public class OverblocksMod extends Mod{
+    Block cb = Blocks.carbonStone;
+    public OverblocksMod(){
+        cb.attributes.set(Attribute.water, 0f);
+    }
     @Override
     public void loadContent(){
         EntityRegistry.register();
+        OBBlocks.load();
     }
 }
---
+++ src/overblocks/content/OBBlocks.java
@@ -0,0 +1,25 @@
+package overblocks.content;
+
+import arc.graphics.*;
+import mindustry.content.*;
+import mindustry.world.*;
+import mindustry.world.blocks.environment.*;
+import mindustry.world.meta.*;
+
+public class OBBlocks {
+    public static Block hotCarbonStone;
+
+    public static void load(){
+        hotCarbonStone = new Floor("hotcarbon-stone"){{
+            attributes.set(Attribute.heat, 0.5f);
+            attributes.set(Attribute.water, -0.5f);
+            wall = Blocks.carbonWall;
+            blendGroup = Blocks.carbonStone;
+            decoration = Blocks.carbonBoulder;
+            variants = 3;
+            emitLight = true;
+            lightRadius = 30f;
+            lightColor = Color.orange.cpy().a(0.15f);
+        }};
+    }
+}
