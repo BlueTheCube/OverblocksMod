bff25a73fb783c70106f3e87962de64c216bc212
Cubical box <calreibozsemimapping@gmail.com>
2024-06-22 17:03:49 +0800

    the jit 8

--- src/overblocks/world/blocks/ReflectionWall.java
+++ src/overblocks/world/blocks/ReflectionWall.java
@@ -5,10 +5,9 @@ import mindustry.entities.*;
 import mindustry.gen.*;
 import mindustry.world.blocks.defense.*;
 
-public class ReflectionWall extends Wall {
-    public float damageMultiplier = 1f;
-    public float damageChance = 0.9f;
-    public ReflectionWall(String name) {
+public class ReflectionWall extends Wall{
+    public float damageMultiplier = 0.9f;
+    public ReflectionWall(String name){
         super(name);
     }
 
@@ -18,16 +17,12 @@ public class ReflectionWall extends Wall {
         super.setStats();
     }
 
-    public class ReflectionWallBuild extends WallBuild {
+    public class ReflectionWallBuild extends WallBuild{
         @Override
-        public boolean collision(Bullet bullet) {
-            if(damageChance > 0){
-                if(!Mathf.chance(damageChance / bullet.damage())) return super.collision(bullet);
-                if(super.collision(bullet) && damageMultiplier > 0f && bullet.owner instanceof Healthc) {
-                    if(bullet.owner() instanceof Unitc uc) {
-                        uc.damage(bullet.damage * damageMultiplier);
-                    }
-                }
+        public boolean collision(Bullet bullet){
+            if(!Mathf.chance(2 / bullet.damage())) return super.collision(bullet);
+            if(super.collision(bullet) && damageMultiplier > 0f && bullet.owner instanceof Healthc c) {
+                c.damage(bullet.damage() * damageMultiplier);
             }
             return super.collision(bullet);
         }
