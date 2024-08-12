a6fe6d835805ae576ddff39a847d52faf746a635
Cubical box <calreibozsemimapping@gmail.com>
2024-08-12 20:20:21 +0800

    the jit 16

---
+++ src/overblocks/type/OBUnitType.java
@@ -0,0 +1,18 @@
+package overblocks.type;
+
+import arc.util.*;
+import mindustry.content.*;
+import mindustry.gen.*;
+import mindustry.type.*;
+
+public class OBUnitType extends UnitType{
+    public @Nullable StatusEffect alwaysStatus;
+    public OBUnitType(String name){
+        super(name);
+    }
+
+    @Override
+    public void update(Unit unit){
+        if(alwaysStatus != null) unit.apply(alwaysStatus);
+    }
+}
