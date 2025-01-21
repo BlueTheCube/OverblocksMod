1c6b1d9a091dde01ca66ac4cf3ae1005a83ccbb2
Cubical box <calreibozsemimapping@gmail.com>
2025-01-21 11:55:58 +0800

    rework

--- src/overblocks/content/OBUnitTypes.java
+++ src/overblocks/content/OBUnitTypes.java
@@ -1,15 +1,19 @@
 package overblocks.content;
 
 import ent.anno.*;
+import mindustry.content.*;
+import mindustry.entities.bullet.*;
 import mindustry.gen.*;
+import mindustry.gen.MechUnit;
 import mindustry.type.*;
+import overblocks.gen.*;
 import overblocks.type.*;
 
 import static ent.anno.Annotations.EntityDef;
 
 public class OBUnitTypes {
 
-    public static @EntityDef({Unitc.class, Mechc.class}) UnitType relayer;
+    public static @EntityDef({Unitc.class, Mechc.class, Dodgec.class}) UnitType relayer;
 
     public static void load(){
     }
--- src/overblocks/type/OBUnitType.java
+++ src/overblocks/type/OBUnitType.java
@@ -1,22 +1,34 @@
 package overblocks.type;
 
+import arc.math.*;
 import arc.util.*;
-import mindustry.content.*;
 import mindustry.gen.*;
 import mindustry.type.*;
+import overblocks.world.meta.*;
+import overblocks.gen.*;
 
-public class OBUnitType extends UnitType{
+public class OBUnitType extends UnitType {
 
     public @Nullable StatusEffect alwaysStatus;
 
     public float dodge = 0f;
 
-    public OBUnitType(String name){
+    @SuppressWarnings("unchecked")
+    public <T extends Unit> OBUnitType(String name, Class<T> type) {
         super(name);
+        constructor = EntityRegistry.content(name, type, n -> EntityMapping.map(this.name));
+        if (constructor == null) throw new IllegalArgumentException("Unit entity class `" + type + "` not registered.");
+    }
+
+
+    @Override
+    public void setStats() {
+        super.setStats();
+        stats.add(OBStats.dodge, Mathf.round(dodge * 100) + "%");
     }
 
     @Override
-    public void update(Unit unit){
+    public void update(Unit unit) {
         if(alwaysStatus != null) unit.apply(alwaysStatus);
     }
 }
