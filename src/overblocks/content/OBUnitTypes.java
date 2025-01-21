f74bc85b97cc09909f966306aa9aaad45fc930d0
Cubical box <calreibozsemimapping@gmail.com>
2025-01-21 11:57:16 +0800

    rework

--- src/overblocks/content/OBUnitTypes.java
+++ src/overblocks/content/OBUnitTypes.java
@@ -16,5 +16,22 @@ public class OBUnitTypes {
     public static @EntityDef({Unitc.class, Mechc.class, Dodgec.class}) UnitType relayer;
 
     public static void load(){
+        relayer = new OBUnitType("relayer", DodgeMechUnit.class){{
+            speed = 0.5f;
+            hitSize = 8f;
+            health = 150;
+            weapons.add(new Weapon("large-weapon"){{
+                reload = 13f;
+                x = 4f;
+                y = 2f;
+                top = false;
+                ejectEffect = Fx.casing1;
+                bullet = new BasicBulletType(2.5f, 9){{
+                    width = 7f;
+                    height = 9f;
+                    lifetime = 60f;
+                }};
+            }});
+        }};
     }
 }
