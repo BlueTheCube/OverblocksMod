47844ff85b6d4a79c7199e79d46cdcf8688cc96c
Cubical box <calreibozsemimapping@gmail.com>
2025-02-01 12:05:55 +0800

    super

--- src/overblocks/type/OBStatusEffect.java
+++ src/overblocks/type/OBStatusEffect.java
@@ -18,11 +18,13 @@ public class OBStatusEffect extends StatusEffect {
 
     @Override
     public void setStats(){
+        super.setStats();
         if(damagePercentage > 0) stats.addPercent(OBStats.damagePercent, damagePercentage);
     }
 
     @Override
     public void update(Unit unit, float time){
+        super.update(unit, time);
         if (damagePercentage > 0) {
             unit.damageContinuousPierce(unit.maxHealth * damagePercentage / 100);
         }
