2bddec18f89df5d10d784d9a89f69e53443ef2f3
Cubical box <calreibozsemimapping@gmail.com>
2025-02-05 11:50:54 +0800

    longk

--- src/overblocks/content/OBStatusEffects.java
+++ src/overblocks/content/OBStatusEffects.java
@@ -12,13 +12,15 @@ public class OBStatusEffects {
     public static StatusEffect dreadRust;
 
     public static void load(){
+
         dreadRust = new OBStatusEffect("dread-rust"){{
             color = OBPal.dreadRust;
             hideDetails = false;
-            damagePercentage = 0.06f;
+            damagePercentage = 0.04f;
             healthMultiplier = 0.85f;
             reloadMultiplier = 0.85f;
             speedMultiplier = 0.75f;
+
             effect = OBFx.dreadRusting;
             transitionDamage = 8f;
         }};
